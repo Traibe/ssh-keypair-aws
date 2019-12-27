cd79893444017993a5aeccabaf346acec6f3b2d2
nickhamilton1980 <nicholasehamilton@gmail.com>
2019-12-27 13:37:04 +1100

    Updte ternary operator

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ module "tls_private_key" {
 }
 
 resource "aws_key_pair" "main" {
-  count = var.create ? 1 : 0
+  count = var.create == true ? 1 : 0
 
   key_name_prefix = "${module.tls_private_key.private_key_name}-"
   public_key      = module.tls_private_key.public_key_openssh
